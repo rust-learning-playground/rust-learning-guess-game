e1af5905b2702d1571033e0c9e9b94c4153b66b1
Sergio Arroutbi <sarroutb@redhat.com>
2022-07-05 10:49:56 +0200

    Guess game
    Signed-off-by: Sergio Arroutbi <sarroutb@redhat.com>

---
+++ src/main.rs
@@ -0,0 +1,29 @@
+use std::io;
+use std::io::Write;
+use rand::Rng;
+use std::cmp::Ordering;
+
+fn main() {
+    println!("Guess the number!");
+
+    loop {
+	print!("Please, input your number:");
+	io::stdout().flush().expect("Failed to flush!");
+	let mut guess = String::new();
+	io::stdin().read_line(&mut guess)
+	    .expect("Failed to read line");
+	let guess: u32 = guess.trim().parse()
+	    .expect("Please, type a number");
+	let secret_number = rand::thread_rng().gen_range(1..11);
+	println!("Secret number: {}", secret_number);
+	println!("You guessed: {}", guess);
+	match guess.cmp(&secret_number) {
+	    Ordering::Less => println!("Too Small!"),
+	    Ordering::Greater => println!("Too Big!"),
+	    Ordering::Equal => {
+		println!("You Win!");
+		break;
+	    },
+	}
+    }
+}
